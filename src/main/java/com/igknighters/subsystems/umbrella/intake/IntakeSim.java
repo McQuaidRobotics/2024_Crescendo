8bf8be834ce059d8c8c441bcaf01fa3d8e088964
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-01-12 14:54:55 -0500

    updated intake sim for speed

--- src/main/java/com/igknighters/subsystems/umbrella/intake/IntakeSim.java
+++ src/main/java/com/igknighters/subsystems/umbrella/intake/IntakeSim.java
@@ -4,6 +4,8 @@ import org.littletonrobotics.junction.Logger;
 
 import com.igknighters.GlobalState;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+
 public class IntakeSim implements Intake {
 
     private final IntakeInputs inputs = new IntakeInputs();
@@ -18,10 +20,14 @@ public class IntakeSim implements Intake {
     @Override
     public void setVoltageOut(double volts) {
         inputs.volts = volts;
+        inputs.radiansPerSecond = (volts / 12.0) * DCMotor.getFalcon500(0).freeSpeedRadPerSec;
     }
 
     @Override
-    public void turnIntake(double radians) {}
+    public void turnIntake(double radians) {
+        inputs.volts = 0.0;
+        inputs.radiansPerSecond = 0.0;
+    }
 
     @Override
     public void periodic() {
