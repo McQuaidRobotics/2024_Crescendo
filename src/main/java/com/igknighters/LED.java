fa570db73d2a70f6f8e721a06166d71c85b4844e
marc611 <maconey@mcqstudent.org>
2024-01-15 19:57:42 -0500

    experimental candle code. needs testing. working on implementing multi-animation functionality

--- src/main/java/com/igknighters/LED.java
+++ src/main/java/com/igknighters/LED.java
@@ -1,5 +1,20 @@
 package com.igknighters;
 
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.ColorFlowAnimation;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.LarsonAnimation;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.RgbFadeAnimation;
+import com.ctre.phoenix.led.SingleFadeAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+import com.ctre.phoenix.led.TwinkleOffAnimation;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
+
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -16,67 +31,97 @@ public class LED {
         return instance;
     }
 
-    public enum LedPatterns {
-        DISABLED,
-        TELEOP,
-        AUTO,
-        TEST,
-        _20S_LEFT,
-        PLACING,
-        BOOTING;
+    private static Animation animDisabled = new ColorFlowAnimation(255, 0, 0, 0, 0.2, 8, Direction.Forward);
+    private static Animation animTeleOp = new FireAnimation(1.0, 0.2, 8, 0.3, 0.3);
+    private static Animation animAuto = new RainbowAnimation(1.0, 0.2, 8);
+    private static Animation animTest = new RgbFadeAnimation(1.0, 0.2, 8);
+    private static Animation anim20sLeft = new SingleFadeAnimation(255, 255, 255);
+    private static Animation animShooting = new TwinkleOffAnimation(255, 255, 0);
+    private static Animation animBooting = new StrobeAnimation(255, 0, 0, 0, 0.2, 8);
+    
+    public enum LedAnimations {
+        DISABLED(animDisabled),
+        TELEOP(animTeleOp),
+        AUTO(animAuto),
+        TEST(animTest),
+        _20S_LEFT(anim20sLeft),
+        SHOOTING(animShooting),
+        BOOTING(animBooting);
 
-        private LedPatterns() {
+        private final Animation anim;
+
+        public Animation getAnim(){
+            return anim;
+        }
+        
+        private LedAnimations(Animation anim) {
+            this.anim=anim;
         }
     }
+    
+    
+
 
     private Timer timer;
     private Double duration;
-    private LedPatterns pattern;
-    private LedPatterns lastPattern;
+    private LedAnimations animation;
+    private LedAnimations lastPattern;
+    private CANdle candle;
+    CANdleConfiguration config;
 
     public LED() {
         this.timer = new Timer();
-        this.pattern = LedPatterns.DISABLED;
-        this.lastPattern = LedPatterns.DISABLED;
+        this.animation = LedAnimations.DISABLED;
+        this.lastPattern = LedAnimations.DISABLED;
         this.duration = 0.0;
-        sendPattern(LedPatterns.DISABLED);
+        candle = new CANdle(0);
+        config = new CANdleConfiguration();
+        config.v5Enabled = true;
+        config.stripType = LEDStripType.RGB;
+        config.brightnessScalar = 1.0;
+        candle.setLEDs(255, 0, 0);
+        sendAnimation(LedAnimations.DISABLED);
 
         new Trigger(DriverStation::isFMSAttached)
                 .and(() -> Math.abs(DriverStation.getMatchTime() - 30.0) < 0.2)
-                .onTrue(new InstantCommand(() -> this.setLed(LedPatterns._20S_LEFT, 2)));
+                .onTrue(new InstantCommand(() -> this.setLed(LedAnimations._20S_LEFT, 2)));
     }
 
-    private void sendPattern(LedPatterns pattern) {
+    
+    private void sendAnimation(LedAnimations anim) {
+        candle.animate(anim.getAnim());
     }
 
-    public void setLed(LedPatterns pattern, double seconds) {
+    public void setLed(LedAnimations anim, double seconds) {
         this.timer.restart();
-        this.pattern = pattern;
+        this.animation = anim;
         this.duration = seconds;
     }
 
-    public void setLed(LedPatterns pattern) {
-        setLed(pattern, Double.POSITIVE_INFINITY);
+
+    public void setLed(LedAnimations anim) {
+        setLed(anim, Double.POSITIVE_INFINITY);
     }
 
     public void setDefault() {
         this.duration = 0.0;
         if (DriverStation.isDisabled()) {
-            this.pattern = LedPatterns.DISABLED;
+            this.animation = LedAnimations.DISABLED;
         } else if (DriverStation.isAutonomous()) {
-            this.pattern = LedPatterns.AUTO;
+            this.animation = LedAnimations.AUTO;
         } else {
-            this.pattern = LedPatterns.TELEOP;
+            this.animation = LedAnimations.TELEOP;
         }
+        sendAnimation(this.animation);
     }
 
     public void run() {
         if (timer.hasElapsed(duration)) {
             setDefault();
         }
-        if (pattern != lastPattern || duration == 0) {
-            sendPattern(pattern);
-            lastPattern = pattern;
+        if (animation != lastPattern || duration == 0) {
+            sendAnimation(animation);
+            lastPattern = animation;
         }
     }
 }
--- src/main/java/com/igknighters/commands/LEDCommands.java
+++ src/main/java/com/igknighters/commands/LEDCommands.java
@@ -2,15 +2,15 @@ package com.igknighters.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import com.igknighters.LED.LedPatterns;
+import com.igknighters.LED.LedAnimations;
 import com.igknighters.LED;
 
 public class LEDCommands {
-    public Command commandSetLED(LedPatterns pattern) {
+    public Command commandSetLED(LedAnimations pattern) {
         return Commands.runOnce(() -> LED.getInstance().setLed(pattern));
     }
 
-    public Command commandSetLED(LedPatterns pattern, double seconds) {
+    public Command commandSetLED(LedAnimations pattern, double seconds) {
         return Commands.runOnce(() -> LED.getInstance().setLed(pattern, seconds));
     }
 }
