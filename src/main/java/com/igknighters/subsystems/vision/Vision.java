3dfb5b7c817586ac49ee8af52b88a0175e5f47bf
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-01-18 00:45:10 -0500

    fixed bug in how april tags are shown on field visualizer

--- src/main/java/com/igknighters/subsystems/vision/Vision.java
+++ src/main/java/com/igknighters/subsystems/vision/Vision.java
@@ -8,6 +8,7 @@ import com.igknighters.constants.ConstValues.kVision;
 import com.igknighters.subsystems.vision.camera.Camera;
 import com.igknighters.util.Tracer;
 
+import java.util.HashSet;
 import java.util.List;
 
 import edu.wpi.first.math.geometry.Pose3d;
@@ -34,6 +35,7 @@ public class Vision extends SubsystemBase {
     @Override
     public void periodic() {
         Tracer.startTrace("VisionPeriodic");
+        HashSet<Integer> seenTags = new HashSet<>();
         for (var camera : cameras) {
             camera.periodic();
             var optEval = camera.evalPose();
@@ -41,24 +43,27 @@ public class Vision extends SubsystemBase {
                 var eval = optEval.get();
 
                 if (Math.abs(eval.pose.getTranslation().getZ() - kDimensions.BELLYPAN_HEIGHT) > kVision.MAX_Z_DELTA) {
-                    continue;
+                    // The cameras height does not change typically, so if it does, it is likely a false positive
+                    GlobalState.submitVisionData(eval, Math.min(eval.ambiguity * 3.0, 1.0));
+                } else {
+                    GlobalState.submitVisionData(eval, eval.ambiguity);
                 }
 
-                GlobalState.submitVisionData(eval, eval.ambiguity);
 
-                GlobalState.modifyField(field -> {
-                    field.getObject("seen_apriltags").setPoses(
-                        eval.apriltags.stream()
-                            .map(tagId -> FieldConstants.APRIL_TAG_FIELD
-                                .getTagPose(tagId)
-                                .orElseGet(() -> new Pose3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d()))
-                                .toPose2d()
-                            )
-                            .toList()
-                    );
-                });
+                seenTags.addAll(eval.apriltags);
             }
         }
+        GlobalState.modifyField(field -> {
+            field.getObject("seen_apriltags").setPoses(
+                seenTags.stream()
+                    .map(tagId -> FieldConstants.APRIL_TAG_FIELD
+                        .getTagPose(tagId)
+                        .orElseGet(() -> new Pose3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d()))
+                        .toPose2d()
+                    )
+                    .toList()
+            );
+        });
         Tracer.endTrace();
     }
 }
