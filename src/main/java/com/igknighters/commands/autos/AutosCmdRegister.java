8401e800edb9ef3304a65cad07242a4a9a6db09a
Maddox-RVS <rynvnskl@yahoo.com>
2024-02-06 19:14:02 -0500

    Registered emtpy named commands so that non-coders can make autos.

--- src/main/java/com/igknighters/commands/autos/AutosCmdRegister.java
+++ src/main/java/com/igknighters/commands/autos/AutosCmdRegister.java
@@ -1,7 +1,15 @@
 package com.igknighters.commands.autos;
 
 import com.igknighters.SubsystemResources.AllSubsystems;
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class AutosCmdRegister {
-    public static void registerCommands(AllSubsystems allSubsystems) {}
+    public static void registerCommands(AllSubsystems allSubsystems) {
+        NamedCommands.registerCommand("PickupPosition", new InstantCommand());
+        NamedCommands.registerCommand("StowPosition", new InstantCommand());
+        NamedCommands.registerCommand("Aim", new InstantCommand());
+        NamedCommands.registerCommand("Shoot", new InstantCommand());
+    }
 }
