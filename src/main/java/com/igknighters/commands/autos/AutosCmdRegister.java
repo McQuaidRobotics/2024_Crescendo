4aabbbdec59649a52ba331b36e157591049c709d
oh-yes-0-fps <foryt.bowan@gmail.com>
2024-02-08 00:15:20 -0500

    the kitchen is on fire

--- src/main/java/com/igknighters/commands/autos/AutosCmdRegister.java
+++ src/main/java/com/igknighters/commands/autos/AutosCmdRegister.java
@@ -1,15 +1,35 @@
 package com.igknighters.commands.autos;
 
 import com.igknighters.SubsystemResources.AllSubsystems;
+import com.igknighters.commands.autos.SpecializedNamedCommands.SpecializedNamedCommand;
 import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class AutosCmdRegister {
+    static class TestCmdMakers {
+        public static InstantCommand makeAim(Double arg) {
+            System.out.println("Aiming at " + arg);
+            return new InstantCommand();
+        }
+    }
+
     public static void registerCommands(AllSubsystems allSubsystems) {
         NamedCommands.registerCommand("PickupPosition", new InstantCommand());
         NamedCommands.registerCommand("StowPosition", new InstantCommand());
         NamedCommands.registerCommand("Aim", new InstantCommand());
         NamedCommands.registerCommand("Shoot", new InstantCommand());
+        try {
+            SpecializedNamedCommands.registerCommand(
+                "Aim",
+                SpecializedNamedCommand.fromMethod(
+                    TestCmdMakers.class.getMethod("makeAim", Double.class),
+                    Double.class
+            ));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        SpecializedNamedCommands.generateSpecialized();
     }
 }
---
+++ src/main/java/com/igknighters/commands/autos/SpecializedNamedCommands.java
@@ -0,0 +1,115 @@
+package com.igknighters.commands.autos;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SpecializedNamedCommands {
+    private static HashMap<String, SpecializedNamedCommand> commands = new HashMap<>();
+
+    private static Object fromString(String s, Class<?> clazz) {
+        if (clazz == Integer.class) {
+            return clazz.cast(Integer.parseInt(s));
+        } else if (clazz == Double.class) {
+            return clazz.cast(Double.parseDouble(s));
+        } else if (clazz == Boolean.class) {
+            return clazz.cast(Boolean.parseBoolean(s));
+        } else {
+            throw new IllegalArgumentException("Unsupported class type");
+        }
+    }
+
+    public record SpecializedNamedCommand(Function<Object[], Command> fn, Class<?>... params) {
+        Command construct(String... args) {
+            if (params.length != args.length) {
+                throw new IllegalArgumentException("Incorrect number of parameters");
+            }
+            Object[] paramObjects = new Object[params.length];
+            for (int i = 0; i < params.length; i++) {
+                paramObjects[i] = params[i].cast(fromString(args[i], params[i]));
+            }
+            return fn.apply(paramObjects);
+        }
+
+        public static SpecializedNamedCommand fromMethod(Method method, Class<?>... params) {
+            return new SpecializedNamedCommand(args -> {
+                try {
+                    return (Command) method.invoke(null, args);
+                } catch (Exception e) {
+                    DriverStation.reportError(e.getMessage(), true);
+                    throw new IllegalArgumentException("Error constructing command");
+                }
+            }, params);
+        }
+
+        public static SpecializedNamedCommand fromClass(Class<?> clazz, Class<?>... params) {
+            return new SpecializedNamedCommand(args -> {
+                try {
+                    return (Command) clazz.getConstructor(params).newInstance(args);
+                } catch (Exception e) {
+                    DriverStation.reportError(e.getMessage(), true);
+                    throw new IllegalArgumentException("Error constructing command");
+                }
+            }, params);
+        }
+    }
+
+    public static void registerCommand(String name, SpecializedNamedCommand command) {
+        commands.put(name, command);
+    }
+
+    public static void generateSpecialized() {
+        Path pathPlannerDir = Filesystem.getDeployDirectory().toPath().resolve("pathplanner");
+        File pathsDir = new File(pathPlannerDir.toFile(), "paths");
+        File autosDir = new File(pathPlannerDir.toFile(), "autos");
+        ArrayList<File> files = new ArrayList<>();
+        files.addAll(List.of(pathsDir.listFiles()));
+        files.addAll(List.of(autosDir.listFiles()));
+
+        for (File file : files) {
+            String contents = "";
+            try {
+                contents = Files.readString(file.toPath());
+            } catch (Exception e) {
+                DriverStation.reportError(e.getMessage(), true);
+            }
+            String[] lines = contents.split("\n");
+            for (String line : lines) {
+                if (line.contains("\"name\": ") && line.contains("(") && line.contains(")")) {
+                    boolean hasCommandName = false;
+                    for (String valid : commands.keySet()) {
+                        if (line.contains(valid)) {
+                            hasCommandName = true;
+                            break;
+                        }
+                    }
+                    if (!hasCommandName) {
+                        continue;
+                    }
+                    String trimmedLine = line.split("\"name\": ")[1].trim().replace(")", "").replace("\"", "");
+                    String commandName = trimmedLine.split("\\(")[0].trim();
+                    String[] args = trimmedLine.split("\\(")[1].split(",");
+                    for (int i = 0; i < args.length; i++) {
+                        args[i] = args[i].trim();
+                    }
+                    if (commands.containsKey(commandName)) {
+                        NamedCommands.registerCommand(commandName, commands.get(commandName).construct(args));
+                    } else {
+                        DriverStation.reportError("Command not found: " + commandName, false);
+                    }
+                }
+            }
+        }
+    }
+}
