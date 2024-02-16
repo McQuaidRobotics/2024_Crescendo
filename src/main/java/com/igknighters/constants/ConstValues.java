7f89de98f1fc118ecbf8fb68c2b97382d8c0c728
Maddox <rynvnskl@yahoo.com>
2024-02-16 10:27:21 -0500

    hopefully computed working offsets, probably not i hate swerve.

--- src/main/java/com/igknighters/constants/ConstValues.java
+++ src/main/java/com/igknighters/constants/ConstValues.java
@@ -242,7 +242,7 @@ public final class ConstValues {
             public static final int CANCODER_ID = 21;
 
             // @DoubleConst(crash = -0.406250, burn = 0.25193)
-            @DoubleConst(crash = -0.406250, burn = 0.404053)
+            @DoubleConst(crash = -0.406250, burn = 0.12692999)
             public static double ROTATION_OFFSET;
 
             public static final Translation2d CHASSIS_OFFSET = new Translation2d(TRACK_WIDTH / 2.0, -TRACK_WIDTH / 2.0);
@@ -257,7 +257,7 @@ public final class ConstValues {
             public static final int CANCODER_ID = 22;
 
             // @DoubleConst(crash = -0.270508, burn = 0.1357)
-            @DoubleConst(crash = -0.270508, burn = -0.479980)
+            @DoubleConst(crash = -0.270508, burn = 0.2607)
             public static double ROTATION_OFFSET;
 
             public static final Translation2d CHASSIS_OFFSET = new Translation2d(-TRACK_WIDTH / 2.0,
@@ -273,7 +273,7 @@ public final class ConstValues {
             public static final int CANCODER_ID = 23;
 
             // @DoubleConst(crash = -0.457764, burn = 0.5774)
-            @DoubleConst(crash = -0.457764, burn = 0.185791)
+            @DoubleConst(crash = -0.457764, burn = 0.9246222)
             public static double ROTATION_OFFSET;
 
             public static final Translation2d CHASSIS_OFFSET = new Translation2d(-TRACK_WIDTH / 2.0, TRACK_WIDTH / 2.0);
@@ -288,7 +288,7 @@ public final class ConstValues {
             public static final int CANCODER_ID = 24;
 
             // @DoubleConst(crash = -0.041504, burn = 1.2545)
-            @DoubleConst(crash = -0.041504, burn = 0.148438)
+            @DoubleConst(crash = -0.041504, burn = 0.8795)
             public static double ROTATION_OFFSET;
 
             public static final Translation2d CHASSIS_OFFSET = new Translation2d(TRACK_WIDTH / 2.0,
