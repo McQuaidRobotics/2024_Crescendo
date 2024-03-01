d71557307ea25ecfe61ff125ad5e05808ab5b93e
Maddox-RVS <rynvnskl@yahoo.com>
2024-02-29 21:31:08 -0500

    Akitified stem disabled.

--- src/main/java/com/igknighters/subsystems/stem/pivot/PivotDisabled.java
+++ src/main/java/com/igknighters/subsystems/stem/pivot/PivotDisabled.java
@@ -1,28 +1,37 @@
 package com.igknighters.subsystems.stem.pivot;
 
+import org.littletonrobotics.junction.Logger;
 import com.igknighters.subsystems.stem.StemPosition;
-
 import edu.wpi.first.math.MathUtil;
 
 public class PivotDisabled implements Pivot {
-    double currentRads = StemPosition.STARTING.pivotRads;
+    private final PivotInputs inputs;
     final double slewRate = (2.37 / 50.0) * 0.75;
+
+    public PivotDisabled() {
+        inputs = new PivotInputs(StemPosition.STARTING.pivotRads);
+    }
 
     @Override
     public double getPivotRadians() {
-        return currentRads;
+        return inputs.radians;
     }
 
     @Override
     public void setPivotRadians(double radians) {
         // var clampedTarget = MathUtil.clamp(radians, kPivot.MIN_ANGLE, kPivot.MAX_ANGLE);
-        currentRads = currentRads + MathUtil.clamp(radians - currentRads, -slewRate, slewRate);
+        inputs.radians = inputs.radians + MathUtil.clamp(radians - inputs.radians, -slewRate, slewRate);
     }
 
     @Override
     public void setVoltageOut(double volts) {
         double percentOut = volts / 12.0;
-        double radiansPerSecond = slewRate * percentOut;
-        currentRads += radiansPerSecond;
+        inputs.radiansPerSecond = slewRate * percentOut;
+        inputs.radians += inputs.radiansPerSecond;
+    }
+
+    @Override
+    public void periodic() {
+        Logger.processInputs("Stem/Pivot", inputs);
     }
 }
