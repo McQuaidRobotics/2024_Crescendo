0cdbbe9261299b79d88282681c5cdeadae8186e9
Maddox-RVS <rynvnskl@yahoo.com>
2024-03-07 19:55:00 -0500

    Started work on motion magic profile sim.

---
+++ src/main/java/com/igknighters/util/MotionMagicProfileSim.java
@@ -0,0 +1,20 @@
+package com.igknighters.util;
+
+public class MotionMagicProfileSim {
+    double cruiseVelo, targetAccel, targetJerk;
+    double velo, accel;
+
+    public MotionMagicProfileSim(double cruiseVelo, double targetAccel, double targetJerk) {
+        this.cruiseVelo = cruiseVelo;
+        this.targetAccel = targetAccel;
+        this.targetJerk = targetJerk;
+    }
+
+    public double calculate(double velo) {
+
+    }
+
+    public void update() {
+
+    }
+}
